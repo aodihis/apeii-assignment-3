37e69ac21c18124bc0783544144c106d25e98015
Iqbal <aodihis@gmail.com>
2026-09-22 12:26:16 +0700

    Update scripts and add models.

---
+++ src/models.ts
@@ -0,0 +1,14 @@
+import { OpenAIClient } from "@anvia/openai";
+
+
+
+const client  =  new OpenAIClient({
+    apiKey: process.env.OPENAI_API_KEY!,
+    baseUrl: process.env.OPENAI_BASE_URL,
+});
+
+export const getModel = (modelId: string) => {
+    return client.completionModel({
+        modelId: modelId || "gpt-5.6-luna"
+    })
+}
